8543dd179b52b3af91f6fbf5ce49451c03309de1
jonas.keidel <jonas.keidel@hetzner.com>
2023-09-04 08:12:58 +0200

    open nvim tree on open

--- init.lua
+++ init.lua
@@ -43,3 +43,9 @@ vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(
 vim.diagnostic.config{
   float={border=_border}
 }
+
+local function open_nvim_tree()
+  require("nvim-tree.api").tree.toggle({ focus = false, find_file = true })
+end
+
+vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
